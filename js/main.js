b160fd02ef859ab531342ed0f134c0d8dfd9f260
jorgealexis07 <201711104@tese.edu.mx>
2022-12-15 10:52:56 -0600

    Obtengo un precio aleatorio y agrego validaciones

--- js/main.js
+++ js/main.js
@@ -7,16 +7,38 @@
 //productosTotal
 //precioTotal
 
-let txtNombre = Document.getElementById("Name");
-let txtNumber = Document.getElementById("Number");
-let total = Document.getElementById("precioTotal");
-
+let txtNombre = document.getElementById("Name");
+let txtNumber = document.getElementById("Number");
+let total = document.getElementById("precioTotal");
 let tabla = document.getElementById("tablaListaCompras");
 let cuerpoTabla = tabla.getElementsByTagName("tbody");
+let btnAgregar = document.getElementById("btn-Agregar");
 
-let btnAgregar = document.getElementById("btnAgrgar");
+//Genera un precio al azar.
+    function getPrecio(){
+        return Math.floor(Math.random()*50*100)/100 ;
+    }//getPrecio
 
-btnAgregar.addEventListener("click",function(event){
+    function validarNombre() {
+        return (txtNombre.value.length>2)?true:false
+    }//validar nombre
+
+    function validarCantidad() {
+        if (txtNumber.value.length==0) {
+            return false;
+        }//if
+        //is not a number: si no es un numero
+        if (isNaN(txtNumber.value) ) {
+            return false;
+        }//if
+        if (parseFloat(txtNumber.value)<=0) {
+            return false;
+        }//if
+        return true;
+    }//validar Cantidad
+
+ btnAgregar.addEventListener("click",function(event){
     event.preventDefault();
+    console.log(validarNombre());
     
-});
+}); 
