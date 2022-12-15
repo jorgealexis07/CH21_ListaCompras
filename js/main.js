331a8a47d9cf07a5ea3a74a2c3701484c1f7390e
jorgealexis07 <201711104@tese.edu.mx>
2022-12-15 13:41:38 -0600

    Se agrego la tabla con los productos y el resumen.

--- js/main.js
+++ js/main.js
@@ -13,6 +13,18 @@ let total = document.getElementById("precioTotal");
 let tabla = document.getElementById("tablaListaCompras");
 let cuerpoTabla = tabla.getElementsByTagName("tbody");
 let btnAgregar = document.getElementById("btn-Agregar");
+let alertValidaciones = document.getElementById("alertValidaciones");
+let alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+let contadorProductos = document.getElementById("contadorProductos");
+let productosTotal = document.getElementById("productosTotal");
+let precioTotal = document.getElementById("precioTotal");
+let idTimeout;
+let precio = 0;
+let cantidad = 0;
+let costoTotal = 0;
+let contador=0;
+let totalEnProductos = 0;
+
 
 //Genera un precio al azar.
     function getPrecio(){
@@ -37,8 +49,70 @@ let btnAgregar = document.getElementById("btn-Agregar");
         return true;
     }//validar Cantidad
 
+  /*   function Validaciones() {
+        validarNombre
+        if (validarNombre() == true && validarCantidad()==true ) {
+            return true;
+        }else{
+            return false;
+        }
+    } */
+
  btnAgregar.addEventListener("click",function(event){
     event.preventDefault();
-    console.log(validarNombre());
+  /*   if (Validaciones() == false) {
+        alertValidaciones.style.display= "block";
+        alertValidaciones.innerHTML = "Valores invalidos";
+    }else{
+        alertValidaciones.style.display= "none";
+    } */
+    clearTimeout(idTimeout);
+    alertValidacionesTexto.innerHTML="";
+    if ((! validarNombre()) || (! validarCantidad())) {
+        let lista= " <ul>";    
+        if (! validarNombre()) {
+            txtNombre.style.border= "red thin solid"
+            lista += "<li> Se debe escribir un nombre válido</li>";
+        }    //validar nombre
+        if (! validarCantidad()) {
+            txtNumber.style.border= "red thin solid"
+            lista += "<li> Se debe escribir una cantidad válida</li>";
+        }    //validar nombre
+        lista+= " </ul>";
+        alertValidacionesTexto.insertAdjacentHTML("beforeend",lista);
+        alertValidaciones.style.display="block";
+        idTimeout =setTimeout(function () {
+            alertValidaciones.style.display="none";    
+        },5000);
+        return false;
+    }//if ! validaciones
+    txtNombre.style.border="";
+    txtNumber.style.border="";
+    alertValidaciones.style.display="none"; 
+    contador++;
+    contadorProductos.innerHTML=contador;
+    let cantidad = parseFloat(txtNumber.value);
+    totalEnProductos += cantidad;
+    productosTotal.innerHTML = totalEnProductos;
+    precio = getPrecio();
+    costoTotal+=precio*cantidad;
+    precioTotal.innerHTML = "$ "+costoTotal.toFixed(2);
+
+    let row = `<tr> 
+    <td>${contador} </td>
+    <td>${txtNombre.value} </td>
+    <td>${txtNumber.value} </td>
+    <td>${precio} </td>
+    </td>`;
+    cuerpoTabla[0].insertAdjacentHTML("beforeend", row);
+        txtNombre.value = "";
+        txtNumber.value = "";
+        txtNombre.focus();
+
+
+
+
+
+
     
-}); 
+});
