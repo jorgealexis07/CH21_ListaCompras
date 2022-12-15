22eb98a4f0ce555af7861691f7bf227376d4b1c2
jorgealexis07 <201711104@tese.edu.mx>
2022-12-15 10:22:38 -0600

    Modificacion de tabla

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-// aquí va el código
+// Campo producto name
+// campo cantidad - number 
+// Boton agregar btnAgregar
+// AlertValidacionesTexto
+//AlertValidaciones
+//contadorPreoductos
+//productosTotal
+//precioTotal
+
+let txtNombre = Document.getElementById("Name");
+let txtNumber = Document.getElementById("Number");
+let total = Document.getElementById("precioTotal");
+
+let tabla = document.getElementById("tablaListaCompras");
+let cuerpoTabla = tabla.getElementsByTagName("tbody");
+
+let btnAgregar = document.getElementById("btnAgrgar");
+
+btnAgregar.addEventListener("click",function(event){
+    event.preventDefault();
+    
+});
